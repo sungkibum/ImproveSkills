6a1da2f0ea54491df1d31e9bf253aac19ac9a937
성기범 <133200703+sungkibum@users.noreply.github.com>
2025-05-22 23:00:37 +0900

    fix: docker 명령어 버전 업데이트에 따른 쉡 스크립트 수정

--- start.sh
+++ start.sh
@@ -4,6 +4,6 @@ COMPOSE_FILES="-f docker/docker-compose.yml -f docker/dev/docker-compose.dev.yml
 ENV_FILE=".env.dev"
 
 echo "Starting Docker Compose with files: $COMPOSE_FILES and env file: $ENV_FILE"
-docker-compose --env-file "$ENV_FILE" $COMPOSE_FILES up --build
+docker compose --env-file "$ENV_FILE" $COMPOSE_FILES up --build
 
 echo "Docker Compose started."
